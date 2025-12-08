85f11761ffb1fe7b602af0243b08444c486c6b15
Satriabp337 <satriabp337@gmail.com>
2025-12-08 19:31:42 +0700

    update transfer file

--- server/ServerController.java
+++ server/ServerController.java
@@ -5,7 +5,7 @@ import common.MessageType;
 import java.util.HashMap;
 
 public class ServerController {
-    //simpan datauser
+    // simpan datauser
     private static HashMap<String, ClientHandler> onlineUsers = new HashMap<>();
 
     public static synchronized void addUser(String username, ClientHandler handler) {
@@ -13,14 +13,19 @@ public class ServerController {
         System.out.println("[SERVER] User registered: " + username);
 
         broadcastMessage("Server ", username + " has joined the chat.");
+
+        broadcastUserList();
     }
 
     public static synchronized void removeUser(String username) {
-        if(onlineUsers.containsKey(username)) {
+        if (onlineUsers.containsKey(username)) {
             onlineUsers.remove(username);
-            System.out.println("[SERVER] user removed : " + username );
+            System.out.println("[SERVER] user removed : " + username);
             broadcastMessage("Server ", username + "has left the chat");
+
+            broadcastUserList();
         }
+
     }
 
     public static synchronized void broadcastMessage(String senderName, String textContent) {
@@ -29,7 +34,7 @@ public class ServerController {
         msg.setContent(textContent);
         msg.setRecipient("ALL");
 
-        for(ClientHandler client : onlineUsers.values()) {
+        for (ClientHandler client : onlineUsers.values()) {
             client.sendMessage(msg);
         }
     }
@@ -38,10 +43,10 @@ public class ServerController {
         ClientHandler targetClient = onlineUsers.get(recipientName);
 
         if (targetClient != null) {
-           Message msg = new Message(MessageType.PRIVATE_CHAT);
+            Message msg = new Message(MessageType.PRIVATE_CHAT);
             msg.setSender(senderName);
             msg.setContent(textContent);
-            msg.setRecipient(recipientName);  
+            msg.setRecipient(recipientName);
 
             targetClient.sendMessage(msg);
             System.out.println("[PRIVATE] " + senderName + " -->" + recipientName);
@@ -50,18 +55,18 @@ public class ServerController {
         }
     }
 
-    public static synchronized void relayFilePacket (Message msg) {
+    public static synchronized void relayFilePacket(Message msg) {
         String target = msg.getRecipient();
 
         if (target.equals("ALL")) {
             for (ClientHandler client : onlineUsers.values()) {
-                if(!client.getUsername().equals(msg.getSender())) {
+                if (!client.getUsername().equals(msg.getSender())) {
                     client.sendMessage(msg);
                 }
             }
         } else {
             ClientHandler targetClient = onlineUsers.get(target);
-            if(targetClient != null) {
+            if (targetClient != null) {
                 targetClient.sendMessage(msg);
             } else {
                 //
@@ -69,10 +74,11 @@ public class ServerController {
         }
     }
 
-    public static synchronized void sendPrivateFile(String senderName, String recipientName, String fileName, byte[] data) {
+    public static synchronized void sendPrivateFile(String senderName, String recipientName, String fileName,
+            byte[] data) {
         ClientHandler targetClient = onlineUsers.get(recipientName);
 
-        if(targetClient != null) {
+        if (targetClient != null) {
             Message msg = new Message(MessageType.FILE_REQUEST);
             msg.setSender(senderName);
             msg.setRecipient(recipientName);
@@ -85,4 +91,32 @@ public class ServerController {
             System.out.println("[GAGAL] Kirim file gagal. User " + recipientName + "offline");
         }
     }
+
+    public static synchronized void broadcastUserList() {
+        // ambil nama user
+        StringBuilder sb = new StringBuilder();
+
+        for (String user : onlineUsers.keySet()) {
+            sb.append(user).append(",");
+
+        }
+
+        // hapus koma terakhir jika ada
+        if (sb.length() > 0) {
+            sb.setLength(sb.length() - 1);
+        }
+
+        String listString = sb.toString();
+
+        Message msg = new Message(MessageType.USER_LIST_UPDATE);
+        msg.setSender("Server");
+        msg.setRecipient("ALL");
+        msg.setContent(listString);
+
+        for (ClientHandler client : onlineUsers.values()) {
+            client.sendMessage(msg);
+        }
+        System.out.println("[SYSTEM] Broadcasting User List : " + listString);
+    }
+
 }
